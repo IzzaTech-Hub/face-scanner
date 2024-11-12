f996942a18a2912e6e82a16c37027eb7ba3e694e
ranasherry <ranasherry94@gmail.com>
2024-11-12 16:35:21 +0500

    implement gemini

---
+++ lib/app/data/celebrity_match.dart
@@ -0,0 +1,37 @@
+class CelebrityMatch {
+  final String name;
+  final String country;
+  final String profession;
+  final double matchPercentage;
+  final String description;
+
+  CelebrityMatch({
+    required this.name,
+    required this.country,
+    required this.profession,
+    required this.matchPercentage,
+    required this.description,
+  });
+
+  // Factory method to create a CelebrityMatch instance from JSON
+  factory CelebrityMatch.fromJson(Map<String, dynamic> json) {
+    return CelebrityMatch(
+      name: json['name'] ?? '',
+      country: json['country'] ?? '',
+      profession: json['profession'] ?? '',
+      matchPercentage: (json['match_percentage'] as num).toDouble(),
+      description: json['description'] ?? '',
+    );
+  }
+
+  // Method to convert a CelebrityMatch instance to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'country': country,
+      'profession': profession,
+      'match_percentage': matchPercentage,
+      'description': description,
+    };
+  }
+}
--- lib/app/modules/controller/celebrity_look_ctl.dart
+++ lib/app/modules/controller/celebrity_look_ctl.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:get/get.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:image_picker/image_picker.dart';
 
 class CelebrityLookCtl extends GetxController {
@@ -23,6 +27,50 @@ class CelebrityLookCtl extends GetxController {
     final XFile? pickedFile = await _picker.pickImage(source: source);
     if (pickedFile != null) {
       selectedImage.value = File(pickedFile.path);
+
+      sendImageToGoogleAI(selectedImage.value!);
     }
+  }
+
+  Future<void> sendImageToGoogleAI(File imgFile) async {
+    final model = GenerativeModel(
+      model: 'gemini-1.5-flash',
+      // model: 'gemini-1.5-flash',
+      apiKey: "AIzaSyD4cCpD7lP-Q9raPF59L8npR8H5NF3pLIo",
+      generationConfig: GenerationConfig(
+        temperature: 0.3,
+        topK: 40,
+        topP: 0.95,
+        maxOutputTokens: 250,
+        responseMimeType: 'text/plain',
+      ),
+    );
+
+    final prompt = '''
+Your are an expert face analyzer. You will be given an image you will have to detect which celebrity best resembles to the person appear in image you have to give your response in following json format.
+
+{
+     "name": "<string>",\n
+     "country": "<string>",\n
+     "profession": "<string>",\n
+     "match_percentage": "<float>",  Range(1,100)
+     "description": "<string>", (describe in what expect the person match to the celebrity. like beard, lips jaw line, eyes, hair, eye brows etc)
+
+     
+}
+
+Note: dont give me any text or disclaimer or note your response should start from { bracket of json structure and end with } json bracket
+''';
+
+    log("Prompt: $prompt");
+    Uint8List imageBytes = await imgFile.readAsBytes();
+    final content = [
+      Content.multi([TextPart(prompt), DataPart('image/jpeg', imageBytes)]),
+    ];
+
+    final response = await model.generateContent(content);
+    log("Response ${response.text}");
+    Map<String, dynamic> jsonMap = jsonDecode(response.text ?? '');
+    log("jsonMap ${jsonMap}");
   }
 }
