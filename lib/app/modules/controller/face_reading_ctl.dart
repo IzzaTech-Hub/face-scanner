91b4f781a9a8c559e3476005ac1457255b113041
Syed Jamal <syedjamal969@gmail.com>
2024-11-13 17:24:23 +0500

    scanner

--- lib/app/modules/controller/face_reading_ctl.dart
+++ lib/app/modules/controller/face_reading_ctl.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
+import 'package:face_scanner/app/utills/images.dart';
+import 'package:face_scanner/app/utills/size_config.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -23,6 +26,38 @@ class FaceReadingCtl extends GetxController {
     final XFile? pickedFile = await _picker.pickImage(source: source);
     if (pickedFile != null) {
       selectedImage.value = File(pickedFile.path);
+      Get.dialog(
+        Container(
+          height: SizeConfig.screenHeight,
+          width: SizeConfig.screenWidth,
+          decoration: BoxDecoration(color: Colors.white),
+          child: AlertDialog(
+            title: Text(''),
+            backgroundColor: Colors.white,
+            content: Stack(
+              children: [
+                Container(
+                  height: SizeConfig.blockSizeVertical * 35,
+                  width: SizeConfig.blockSizeHorizontal * 70,
+                  decoration: BoxDecoration(),
+                  child: Image.asset(
+                    AppImages.scanner,
+                    color: Colors.teal,
+                  ),
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Get.back(); // Close the dialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        ),
+      );
     }
   }
 }
