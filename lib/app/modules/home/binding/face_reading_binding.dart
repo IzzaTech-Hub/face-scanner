5bcef94c78d8c9349f2bb5d5dc6a7dc50c5c546d
ranasherry <ranasherry94@gmail.com>
2024-11-14 16:45:36 +0500

    conflits

--- lib/app/modules/home/binding/face_reading_binding.dart
+++ lib/app/modules/home/binding/face_reading_binding.dart
@@ -1,4 +1,4 @@
-import 'package:face_scanner/app/modules/controller/face_reading_ctl.dart';
+import 'package:face_scanner/app/modules/home/controller/face_reading_ctl.dart';
 import 'package:get/get.dart';
 
 class FaceReadingBinding extends Bindings {
