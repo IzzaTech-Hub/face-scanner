99acf4c3d89fcd9821cee88141f44f4a33dbfb8d
ranasherry <ranasherry94@gmail.com>
2024-11-26 12:38:52 +0500

    rcvariables

--- lib/app/utills/rc_variables.dart
+++ lib/app/utills/rc_variables.dart
@@ -1,5 +1,6 @@
 class RCVariables {
   static const customeSearchKey = "AIzaSyB-bJtmzJNUJug6k1kIvkbDb1f0MhkYcuI";
-  static const GeminiAPIKey = "AIzaSyD4cCpD7lP-Q9raPF59L8npR8H5NF3pLIo";
+  static const GeminiAPIKey = "AIzaSyDWOqbYXZ_BbNmivWxa4rbPysshKUrAmSA";
+  // static const GeminiAPIKey = "AIzaSyD4cCpD7lP-Q9raPF59L8npR8H5NF3pLIo";
   static const customeSearchcx = "f040e1d0e21e74322";
 }
