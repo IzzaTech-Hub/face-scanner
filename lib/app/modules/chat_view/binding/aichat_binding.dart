ead0e7f7345356395d3bfdd9a1c85704d3cda154
Syed Jamal <syedjamal969@gmail.com>
2024-12-20 15:44:06 +0500

    markdown

--- lib/app/modules/chat_view/binding/aichat_binding.dart
+++ lib/app/modules/chat_view/binding/aichat_binding.dart
@@ -1,6 +1,5 @@
+import 'package:face_scanner/app/modules/chat_view/controller/aichat_controller.dart';
 import 'package:get/get.dart';
-
-import '../controllers/aichat_controller.dart';
 
 class AichatBinding extends Bindings {
   @override
